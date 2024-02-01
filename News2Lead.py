7805479b28f5de654ea837ffc63eaf57e0c5baf9
SKalahast <112964897+SKalahast@users.noreply.github.com>
2024-02-01 09:04:55 +0530

    Update News2Lead.py

--- News2Lead.py
+++ News2Lead.py
@@ -53,7 +53,7 @@ def news_2_leads(client,start_date,end_date):
     url = 'https://www.google.com/'
     
     # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome(options=options, service=get_webdriver_service())
+    driver = webdriver.Chrome(options=options, service=get_webdriver_service(logpath=logpath))
     # driver = webdriver.Chrome()
     driver.get("https://www.google.com/")
     driver.quit()
